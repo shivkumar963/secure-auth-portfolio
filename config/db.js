91363d3a4f831aa1d6236e79cf12f744a2dfe6f5
Shiv Kumar <kshiv397499@gmail.com>
2025-12-12 01:03:30 +0530

    update code

--- config/db.js
+++ config/db.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose');
 
 // Dono naam support karenge: MONGO_URI ya MONGODB_URI
-const MONGO_URI =
-  process.env.MONGO_URI ||
-  process.env.MONGODB_URI ||   // <- ye line add
+const MONGO_URL =
+  process.env.MONGO_URL ||
+  process.env.MONGODB_URL ||   // <- ye line add
   'mongodb://0.0.0.0/men';
 
-mongoose.connect(MONGO_URI)
-  .then(() => {
-    console.log("✅ Connected to Database");
-  })
-  .catch((err) => {
-    console.error("❌ Error in Database", err);
-  });
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("🔥 MongoDB Atlas Connected Successfully"))
+  .catch(err => console.error("❌ MongoDB Connection Failed:", err));
 
 module.exports = mongoose;
